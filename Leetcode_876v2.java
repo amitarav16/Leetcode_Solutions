dc7423e4a3043bf9f31168cd891deffffb444911
amitarav16 <amitkumar26201@gmail.com>
2020-07-29 12:06:41 +0530

    Middle of the Linked List using 2 pointer approach

---
+++ Leetcode_876v2.java
@@ -0,0 +1,11 @@
+public class Leetcode_876v2{
+	public ListNode middleNode(ListNode head) {
+        ListNode slow_ptr = head;
+        ListNode fast_ptr = head;
+        while(fast_ptr !=null && fast_ptr.next != null){
+            slow_ptr = slow_ptr.next;
+            fast_ptr = fast_ptr.next.next;
+        }
+        return slow_ptr;
+    }
+}
