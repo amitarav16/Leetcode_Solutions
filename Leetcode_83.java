ee509905f42376be8d2947344cd1cf67e075dd3c
amitarav16 <amitkumar26201@gmail.com>
2020-07-30 13:31:05 +0530

    Remove Duplicates from Sorted List

---
+++ Leetcode_83.java
@@ -0,0 +1,15 @@
+public class Leetcode_83{
+	 public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while(temp != null && temp.next != null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
+            }
+            else{
+            temp = temp.next;
+        }
+            
+        }
+        return head;
+    }
+}
